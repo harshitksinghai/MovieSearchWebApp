b236dc2e48bd90c2a0bdb04f320ae05a7e715941
Harshit Kumar Singhai <hksinghai@ymsl.in>
2025-03-10 15:20:13 +0530

    remove unused imports

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,8 +2,6 @@ import { Outlet } from 'react-router-dom'
 import './i18n/config.ts'
 import ThemeProviderWrapper from './theme/ThemeProviderWrapper.tsx'
 import Footer from './components/Footer.tsx'
-import { persistor } from './app/store.ts';
-import { PersistGate } from 'redux-persist/integration/react';
 
 import { useEffect } from "react";
 import { useAppDispatch, useAppSelector } from "./app/hooks.ts";
@@ -21,19 +19,19 @@ function App() {
   }, [dispatch, userId]);
 
   return (
-    
-            <ThemeProviderWrapper>
-              <div style={{
-                display: 'flex',
-                flexDirection: 'column',
-                minHeight: '100vh'
-              }}>
-                <main style={{ flex: '1 0 auto' }}>
-                  <Outlet />
-                </main>
-                <Footer />
-              </div>
-            </ThemeProviderWrapper>
+
+    <ThemeProviderWrapper>
+      <div style={{
+        display: 'flex',
+        flexDirection: 'column',
+        minHeight: '100vh'
+      }}>
+        <main style={{ flex: '1 0 auto' }}>
+          <Outlet />
+        </main>
+        <Footer />
+      </div>
+    </ThemeProviderWrapper>
 
   )
 }
